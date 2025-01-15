1438b7d55df9c30438b9835a9aac81d1f7a8188e
MajdIssa34 <issamajd00@gmail.com>
2025-01-15 11:30:36 +1100

    Started implementing the user functionalities.

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
 import dotenv from "dotenv";
 import connectMongoDB from "./db/connectMongoDB.js";
 import cookieParser from "cookie-parser";
@@ -15,6 +17,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+
 
 
 app.listen(8000, () => {
